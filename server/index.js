f014d36a4f59c97a5587ee4ed8437fa55544531c
nishuya <prathamyadav1817@gmail.com>
2026-04-28 00:14:34 +0530

    backend url change

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 
 app.use(express.json());
 
-const allowedOrigins = ["http://localhost:1234"];
+const allowedOrigins = ["https://earnest-twilight-9dab59.netlify.app"];
 
 app.use(cors({
   origin: (origin, callback) => {
